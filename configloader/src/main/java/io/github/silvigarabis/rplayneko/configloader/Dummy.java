2686380616c8c8f88118d407ed0aece49ffcca7b
Silvigarabis <73000464+Silvigarabis@users.noreply.github.com>
2024-06-14 10:55:09 +0800

    修复依赖关系(⁠｡⁠・⁠/⁠/⁠ε⁠/⁠/⁠・⁠｡⁠)

---
+++ configloader/src/main/java/io/github/silvigarabis/rplayneko/configloader/Dummy.java
@@ -0,0 +1,20 @@
+package io.github.silvigarabis.rplayneko.configloader;
+
+import io.github.silvigarabis.rplayneko.RPlayNekoConfig;
+import java.util.Map;
+import java.io.InputStream;
+
+// 保留一份代码便于复制
+
+public final class Dummy {
+    public static boolean isAvailable(){
+        return false;
+    }
+    public static RPlayNekoConfig loadCoreConfig(InputStream in){
+        throw new RuntimeException();
+    }
+    public static Map<String, String> loadMessageConfig(InputStream in){
+        throw new RuntimeException();
+    }
+    private Dummy(){ }
+}
